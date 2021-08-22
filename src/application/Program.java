6cadad925f732da36d0577365bf91d5b057ae3fd
higoramorim <1810042@aluno.univesp.br>
2021-08-22 13:26:21 -0300

    inicio

---
+++ src/application/Program.java
@@ -0,0 +1,17 @@
+package application;
+
+public class Program {
+
+	public static void main(String[] args) {
+		ContaPoupanca contaPoupanca = new ContaPoupanca("Zezinho", 999, 3580, 1000, 0.05, 30, 3);
+		contaPoupanca.meuSaldo(30, 0.05, 3, 30);
+		System.out.println("Cliente Conta Poupança: idCliente: " + contaPoupanca.getId() + ", nomeCliente: " + contaPoupanca.getNomeCliente() + ", numConta: " + contaPoupanca.getNumeroConta() + ", tempoDepositadoMeses: " + contaPoupanca,getTempoDepositadoMeses() + ", txJuros: " + contaPoupanca.getTxJuros());
+
+		ContaSalario contaSalario = new ContaSalario("Huguinho", 888, 3581, 1000, 3500);
+		contaSalario.sacar(300);
+		
+		ContaCorrente contaCorrente = new ContaCorrente("Fulaninho", 777, 3582, 1000, 2000);
+		contaCorrente.sacar(300);
+	}
+
+}
